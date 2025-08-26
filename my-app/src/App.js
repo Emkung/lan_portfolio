c473671bd72f11160c33b9ce19c77fc2a5ec16a1
HsiaolanKung <hsiaolankung@gmail.com>
2025-08-25 22:15:41 -0400

    got a tool bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { AppBar, Toolbar, Typography, Button, Stack} from '@mui/material'
+import AppAppBar from './components/AppAppBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <AppBar position="static">
+          <Toolbar color = 'black'>
+            <Typography variant="title" color="inherit">
+           </Typography>
+           <Stack spacing={2} direction="row-reverse">
+            <Button variant='outlined' color='white'>Games</Button>
+            <Button variant='outlined' color='white'>Event Organization</Button>
+            <Button variant='outlined' color='white'>Illustration</Button>
+            <Button variant='outlined' color='white'>About</Button>
+           </Stack>
+          </Toolbar>
+        </AppBar> */}
+        <AppAppBar></AppAppBar>
+      </div>
+    );
+  }
 }
-
 export default App;
